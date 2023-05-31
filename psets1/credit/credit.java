63be57572c228f68c66ac4f793ae5f1a1ec63237
Thilanka Weerasekara <thilankapathum@gmail.com>
2023-05-31 22:01:00 +0530

    checked min and max digit count validity

--- psets1/credit/credit.java
+++ psets1/credit/credit.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+// Amex 15 Digits: 34 37
+// MasterCard 16 digits: 51 52 53 54 55
+// Visa 13 16 digits: 4
+
 class Credit{
 
     public static void main(String[] args) {
@@ -15,10 +19,10 @@ class Credit{
         sc.close();
 
         long max = 100000000000000000L; // Maximum value that a credit card can get/10
-        //long num = 54003600000000014L; 378282246310005 30569309025904 4222222222222
+        long min = 1000000000000L; // Min value that a credit card can get/10
 
         // Check for long inputs < 16 digits:
-        if (num*10<max){
+        if (num*10<max && num>min){
 
             int numberArray[] = new int[16];
             int k = 0;
@@ -34,47 +38,24 @@ class Credit{
                 k++;
             }
 
-            // Print digits of number in reverse order:
-            System.out.print("NUMBER ARRAY: ");
-            for(int l=0;l<16;l++){
-                System.out.print(numberArray[l]);
-            }
-
-
             // Calculate checksum:
-            //int productsArray[]=new int[8];
             int othersSum = 0, numberSumP=0;
 
             // Calulate sum of products of odd locations and store in numberSumP:
             for(int m=1;m<16;m+=2){
 
                 if(2*numberArray[m]<10){
-                    //productsArray[p] = 2*numberArray[m];
-                    numberSumP += 2*numberArray[m];
+                    numberSumP += 2*numberArray[m]; 
                 }else{
-                    //productsArray[p]= 2*numberArray[m]-9;
-                    numberSumP += 2*numberArray[m]-9;
-
+                    numberSumP += 2*numberArray[m]-9; // Check for multiplications > 10 to add those digits
                 }
-
             }
-            //System.out.print("Products Sum: ");
-
-            // NOT NEEDED:
-        /*    for(int i=0;i<8;i++){
-                System.out.print(productsArray[i] + "-");
-                productsSum += productsArray[i];
-            } */
 
             // Calculate sum of even locations and store in othersSum:
             for(int i=0;i<16;i+=2){
                 othersSum += numberArray[i];
-                //numberSumS += numberArray[i];
             }
 
-            //System.out.println("Products Sum: "+productsSum);
-            //System.out.println("Others Sum: "+othersSum);
-            //System.out.println("Total: " + (othersSum+productsSum));
             System.out.println("Total NUMBERSUM: " + (numberSumP+othersSum));
 
         }else{
