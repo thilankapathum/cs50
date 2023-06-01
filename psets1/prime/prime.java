a5ff50868bad715591ee42def51e510980929ac0
Thilanka Weerasekara <thilankapathum@gmail.com>
2023-06-01 22:24:05 +0530

    prime numbers upto user's number is done

---
+++ psets1/prime/prime.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class Prime
+{
+    public static void main(String[] args)
+    {
+
+        // Create Scanner object to get user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        String userInputString = sc.nextLine();
+
+        // Parse user input from String to int
+        int userInput = Integer.parseInt(userInputString);
+
+        sc.close();
+
+        // Initialize Boolean catch modulo (divisibility)
+        boolean checkPrime = false;
+
+        // Checking each number from 1 to userInput
+        for(int j = 1; j < userInput; j++)
+        {
+            // Initialize Boolean for each loop
+            checkPrime = false;
+
+            // Check whether the number is divisible by its predecessors
+            for(int k = 2; k < j; k++)
+            {
+
+                // Checking modulo for divisibility
+                if(j%k == 0)
+                {
+                    checkPrime = true;
+
+                    // breaking 'for' loop to prevent unnecessary repetitions
+                    break;
+                }
+            }
+
+            // Print number as a prime if modulo were not zero
+            if(!checkPrime)
+            {
+                System.out.println(j);
+            }
+        }
+    }
+}
