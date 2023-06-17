c9ced3df9f32f71c170224fa536db4c058d5367c
Thilanka Weerasekara <thilankapathum@gmail.com>
2023-06-17 19:47:29 +0530

    vote & print works for static data

--- psets3/plurality.java
+++ psets3/plurality.java
@@ -5,7 +5,7 @@ class Plurality{
         int candidatesCount = 3; // Get number of Candidates
 
         //String[] candidatesName = new String[candidatesCount];
-        String[] candidatesName = {"Alice", "Charile", "Bob"};
+        String[] candidatesName = {"Alice", "Charlie", "Bob"};
 
         Candidate[] candidates = new Candidate[candidatesCount]; // Create candidates array
 
@@ -14,17 +14,19 @@ class Plurality{
             candidates[i].name = candidatesName[i];
         }
 
-        int votersCount = 3;
+        int votersCount = 6;
 
-        String[] votersPreference = {"Alice", "Alice", "Bob"};
+        String[] votersPreference = {"Alice", "Alice", "Bob", "Bob", "Charlie", "Charlie"};
 
         for(int j = 0; j < votersCount; j++){
 
-            for(int i = 0; i < candidatesCount; i++){
+            /* for(int i = 0; i < candidatesCount; i++){
                 if(votersPreference[j] == candidates[i].name){
                     candidates[i].votes += 1;
                 }
-            }
+            } */
+
+            vote(candidates,votersPreference[j]);
             
         }
         
@@ -33,7 +35,54 @@ class Plurality{
         for(int i = 0; i < candidatesCount; i++){
             System.out.println(candidates[i].name + candidates[i].votes);
         }
+
+        printWinner(candidates);
     }
+
+    // Method to cast Vote
+    static void vote(Candidate[] candidates, String candidateName){
+
+        int candidateCount = candidates.length;
+        boolean candidateAvailable = false;
+
+        // Check for each candidate
+        for(int i = 0; i < candidateCount; i++){
+
+            // checking the voter input & candidate name
+            if(candidateName == candidates[i].name){
+                candidates[i].votes += 1;
+                candidateAvailable = true;
+            }
+                
+        }
+
+        // Voter input mismatch
+        if(!candidateAvailable){
+            System.out.println("Candidate is Unavailable");
+        }
+    }
+
+    // Print Winner(s)
+    static void printWinner(Candidate[] candidates){
+
+        int candidateCount = candidates.length;
+        int maxVotes = 0;
+
+        // Finding the maximum votes
+        for(int i = 0; i < candidateCount; i++){
+            if(candidates[i].votes > maxVotes){
+                maxVotes = candidates[i].votes;
+            }
+        }
+
+        // Printing candidates with max votes
+        for(int i = 0; i < candidateCount; i++){
+            if(maxVotes == candidates[i].votes){
+                System.out.println("Winner is: " + candidates[i].name);
+            }
+        }
+    }
+
 }
 
 class Candidate{
